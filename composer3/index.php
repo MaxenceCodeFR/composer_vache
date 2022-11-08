d9e91cc97bb141aa0f0cb436153353f1c9315db9
MaxenceCodeFR <maxence.hattabi@gmail.com>
2022-11-08 09:10:55 +0100

    La vache

--- composer3/index.php
+++ composer3/index.php
@@ -10,4 +10,22 @@ $output = $bessie->say();
 echo $output;
 
 // or just echo the object for direct output
-echo $bessie;
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <pre><?= $bessie; ?></pre>
+</body>
+
+</html>
